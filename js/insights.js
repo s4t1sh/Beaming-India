be296707740821ecec5462b10f489e53e2c7085d
V Satish <vasettisatish@gmail.com>
2024-01-03 16:23:50 +0530

    inights

--- js/insights.js
+++ js/insights.js
@@ -15,16 +15,66 @@ document.addEventListener("DOMContentLoaded", function () {
             // Get the target element
             const targetElement = document.querySelector(this.getAttribute('href'));
 
-            // Reset the scroll position to the top of the page
-            window.scrollTo({
-                top: 0,
-                behavior: 'auto'
-            });
-
             // Use scrollIntoView with smooth behavior
             targetElement.scrollIntoView({
                 behavior: 'smooth'
             });
         });
     });
+
+    window.addEventListener('scroll', function () {
+        var leftCol = document.getElementById('leftCol');
+        var rightCol = document.getElementById('rightCol');
+
+        var scrollY = window.scrollY;
+        
+        // Stick left column after 3 scrolls
+        if (scrollY >= 300) {
+            leftCol.classList.add('sticky');
+        } else {
+            leftCol.classList.remove('sticky');
+        }
+
+        // Stick right column after 1 scroll
+        if (scrollY >= 100) {
+            rightCol.classList.add('sticky');
+        } else {
+            rightCol.classList.remove('sticky');
+        }
+    });
+
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // Function to add or remove the active-content class based on the section visibility
+    function updateActiveContent() {
+        var links = document.querySelectorAll('.contents');
+
+        links.forEach(function (link) {
+            var targetId = link.getAttribute('href').substring(1); // Remove the '#' from href
+            var targetSection = document.getElementById(targetId);
+
+            if (targetSection && isElementInViewport(targetSection)) {
+                link.classList.add('active-content');
+            } else {
+                link.classList.remove('active-content');
+            }
+        });
+    }
+
+    // Event listener for scroll events
+    window.addEventListener('scroll', updateActiveContent);
+
+    // Initial check on page load
+    window.addEventListener('load', updateActiveContent);
 });
+
+
+   
