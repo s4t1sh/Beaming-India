538cfd4577100aaac83756a05c831c80b3868e2d
V Satish <vasettisatish@gmail.com>
2024-01-03 11:57:58 +0530

    navbar

--- js/menu.js
+++ js/menu.js
@@ -2,17 +2,18 @@ document.getElementById('mobile-menu').addEventListener('click', function() {
     var navMenu = document.querySelector('.nav-menu');
     var menuToggle = document.querySelector('.menu-toggle');
   
-    if (navMenu.style.top === '0%') {
-      navMenu.style.top = '-100%';
+    if (navMenu.style.top === '0') {
+      navMenu.style.height = '0';
+      navMenu.style.top = '-100vh';
     } else {
-      navMenu.style.top = '0%';
+      navMenu.style.top = '0';
     }
   
     menuToggle.classList.toggle('menu-open');
   });
   
   document.getElementById('close-menu').addEventListener('click', function() {
-    document.querySelector('.nav-menu').style.top = '-100%';
+    document.querySelector('.nav-menu').style.top = '-100vh';
     document.querySelector('.menu-toggle').classList.remove('menu-open');
   });
   
